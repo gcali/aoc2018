ffa9f222195db5cb3858a850ec0a280e3c4d2ef8
gcali <gcali.git@gmail.com>
2019-01-04 00:54:16 +0100

    Add production configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-    configureWebpack: {
-        devtool: 'source-map'
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        baseUrl: '/aoc2018',
+        configureWebpack: {
+            devtool: 'source-map'
+        }
+    }
+}
+else {
+    module.exports = {
+        configureWebpack: {
+            devtool: 'source-map'
+        }
     }
 }
