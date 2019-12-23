a120c372cfa597263dadd94e0be3e0f954b5ef1a
gicali <gcali.git@gmail.com>
2019-12-23 22:03:42 +0100

    First star of stoichiometry done, trying second!

--- src/entries/single-entries/space-stoichiometry.ts
+++ src/entries/single-entries/space-stoichiometry.ts
@@ -1,5 +1,5 @@
 import { entryForFile } from "../entry";
-import { setTimeoutAsync, mapAsync } from '../../support/async';
+import { setTimeoutAsync, mapAsync } from "../../support/async";
 
 interface Element {
     name: string;
@@ -29,15 +29,15 @@ function parseElement(raw: string): Element {
 }
 
 function parseLine(line: string): Chain {
-    const [left, right] = line.split("=>").map(e => e.trim());
+    const [left, right] = line.split("=>").map((e) => e.trim());
 
-    const needs = left.split(",").map(e => e.trim()).map(parseElement);
+    const needs = left.split(",").map((e) => e.trim()).map(parseElement);
     const target = parseElement(right);
     return { needs, target };
 }
 
 function parseLines(lines: string[]): Chain[] {
-    return lines.filter(l => !l.startsWith("--")).map(l => l.trim()).filter(l => l.length > 0).map(parseLine);
+    return lines.filter((l) => !l.startsWith("--")).map((l) => l.trim()).filter((l) => l.length > 0).map(parseLine);
 }
 
 const targetName = "FUEL";
@@ -48,7 +48,7 @@ interface OrePrice {
     youNeed: number;
 }
 
-type CalculatedPrices = { [key: string]: OrePrice };
+interface CalculatedPrices { [key: string]: OrePrice; }
 
 class Remaining {
     private readonly _remainingMap: { [key: string]: number } = {};
@@ -60,7 +60,7 @@ class Remaining {
 
     public askFor(e: Element): number {
         this.ensure(e.name);
-        const howManyCanTake = Math.max(this._remainingMap[e.name] - e.amount, 0);
+        const howManyCanTake = Math.min(this._remainingMap[e.name], e.amount);
         this._remainingMap[e.name] -= howManyCanTake;
         return howManyCanTake;
     }
@@ -82,39 +82,46 @@ async function oreRequirement(target: Element, c: Chain[], r: Remaining, d?: Deb
         await intLog("ID: " + closed);
         await intLog(s);
     };
-    await log("Trying to build:")
-    await log(target)
+    await log("Trying to build:");
+    await log(target);
     if (target.name === baseName) {
-        await log("Trying to ore, done with requirements:")
+        await log("Trying to build ore, done with requirements:");
         const res = { toCreate: target.amount, youNeed: target.amount };
-        await log(res)
+        await log(res);
         return res;
     }
+    await log("Trying with remainings");
+    await log((r as any)._remainingMap);
     const howManyRequired = target.amount - r.askFor(target);
     if (howManyRequired === 0) {
-        await log("Everything found in remaining")
+        await log("Everything found in remaining");
         return {
             toCreate: target.amount,
             youNeed: 0
         };
     }
-    await log("Only some found in remaining, new required:")
+    await log("Only some found in remaining, new required:");
     await log(howManyRequired);
-    const chain = c.filter(e => e.target.name === target.name)[0];
+    await log("New remaining:");
+    await log((r as any)._remainingMap);
+    const chain = c.filter((e) => e.target.name === target.name)[0];
     const howManyNeedToBuild = Math.ceil(howManyRequired / chain.target.amount);
     await log(`Recipe says how to build ${chain.target.amount}, need ${howManyRequired}, building ${howManyNeedToBuild}`);
-    const needs = chain.needs.map(n => ({ name: n.name, amount: n.amount * howManyNeedToBuild }));
+    const needs = chain.needs.map((n) => ({ name: n.name, amount: n.amount * howManyNeedToBuild }));
     await log(`New needs:`);
-    await log(needs);
-    const prices = await mapAsync(needs, async n => ({ price: await oreRequirement(n, c, r, d), element: n }));
-    prices.forEach(p => {
-        if (p.price.toCreate > p.element.amount) {
-            r.add({ name: p.element.name, amount: p.price.toCreate - p.element.amount });
-        }
-    });
+    await log(JSON.stringify(needs));
+    const prices = await mapAsync(needs, async (n) => ({ price: await oreRequirement(n, c, r, d), element: n }));
+    // prices.forEach((p) => {
+    //     if (p.price.toCreate > p.element.amount) {
+    //         r.add({ name: p.element.name, amount: p.price.toCreate - p.element.amount });
+    //     }
+    // });
+    r.add({ name: target.name, amount: howManyNeedToBuild * chain.target.amount - howManyRequired });
+    await log("Updated remaining:");
+    await log((r as any)._remainingMap);
     const finalResult = {
         toCreate: target.amount,
-        youNeed: prices.map(p => p.price.youNeed).reduce((a, b) => a + b)
+        youNeed: prices.map((p) => p.price.youNeed).reduce((a, b) => a + b)
     };
     await log("Final result:");
     await log(finalResult);
@@ -122,62 +129,10 @@ async function oreRequirement(target: Element, c: Chain[], r: Remaining, d?: Deb
 }
 
 
-function basePriceGenerator(): CalculatedPrices {
-    const d: CalculatedPrices = {};
-    d[baseName] = { toCreate: 1, youNeed: 1 };
-    return d;
-}
-
-function findRequirements(e: Element, c: Chain[]): Element[] {
-    if (e.name === baseName) {
-        return [e];
-    } else {
-        const target = c.filter(e => e.target.name === e.target.name)[0];
-        const needs = target.needs;
-        const coeff = Math.ceil(e.amount / target.target.amount);
-        const newNeeds: Element[] = needs.map(innerE => ({ name: innerE.name, amount: innerE.amount * coeff }));
-        return newNeeds;
-    }
-}
-
 interface Debug {
     pause?: () => Promise<void>;
     log?: (s: any) => Promise<void>;
 }
-
-async function getPrice(target: string, c: Chain[], d?: Debug): Promise<number> {
-    const targetChain = c.filter(e => e.target.name === target)[0];
-    let needs = targetChain.needs;
-    if (targetChain.target.amount > 1) {
-        needs.map(n => ({ name: n.name, amount: n.amount * targetChain.target.amount }));
-    }
-    console.log("Needs:", needs);
-    if (d && d.log) {
-        await d.log(needs.map(n => n.name).join(" "));
-        await setTimeoutAsync(1000);
-    }
-    while (needs.length > 1 && needs[0].name !== baseName) {
-        needs = needs
-            .flatMap(n => findRequirements(n, c));
-        if (d && d.log) {
-            await d.log(needs.map(n => n.name).join(" "));
-            await setTimeoutAsync(1000);
-        } else if (d && d.pause) {
-            await d.pause();
-        }
-        const groups: { [key: string]: Element[] } = {};
-        needs.forEach(e => {
-            if (groups[e.name]) {
-                groups[e.name].push(e);
-            } else {
-                groups[e.name] = [e];
-            }
-        });
-        needs = Object.values(groups).map(e => e.reduce((a, b) => ({ name: a.name, amount: a.amount + b.amount })));
-    }
-    return needs[0].amount;
-}
-
 
 export const spaceStoichiometry = entryForFile(
     async ({ lines, outputCallback, pause, isCancelled }) => {
@@ -185,11 +140,49 @@ export const spaceStoichiometry = entryForFile(
         await outputCallback(serializeChains(chains));
         // const amount = await getPrice(targetName, chains, { log: outputCallback, pause: pause });
         const remain = new Remaining();
-        const amount = await oreRequirement(chains.filter(e => e.target.name === targetName)[0].target, chains, remain, { log: outputCallback, pause });
+        // const amount = await oreRequirement(chains.filter((e) => e.target.name === targetName)[0].target, chains, remain, { log: outputCallback, pause });
+        const amount = await oreRequirement(chains.filter((e) => e.target.name === targetName)[0].target, chains, remain);
         await outputCallback(amount);
-        await outputCallback("Remaining:")
+        await outputCallback("Remaining:");
         await outputCallback((remain as any)._remainingMap);
     },
     async ({ lines, outputCallback, pause, isCancelled }) => {
+        const chains = parseLines(lines);
+        let current: number = 0;
+        let targetAmount = 1;
+        const storageOres = 1000000000000;
+        const target = chains.filter((e) => e.target.name === targetName)[0].target;
+        while (current < storageOres) {
+            targetAmount *= 2;
+            target.amount = targetAmount;
+            current = (await oreRequirement(target, chains, new Remaining())).youNeed;
+            await outputCallback("Current guess:");
+            await outputCallback({ targetAmount, current });
+        }
+        let under = Math.floor(targetAmount / 2);
+        let over = targetAmount;
+        let guess = Math.ceil((under + over) / 2);
+        while (under < over) {
+            target.amount = guess;
+            const needed = (await oreRequirement(target, chains, new Remaining())).youNeed;
+            if (needed === storageOres) {
+                break;
+            } else if (needed > storageOres) {
+                over = guess;
+            } else if (needed < storageOres) {
+                under = guess;
+            }
+            guess = Math.ceil((under + over) / 2);
+            await outputCallback("New status:");
+            await outputCallback({ under, over, guess });
+            if (guess === under) {
+                break;
+            }
+        }
+        await outputCallback("You can create " + guess);
+        // const amount = await oreRequirement(target, chains, remain);
+        // await outputCallback(amount);
+        // await outputCallback("Remaining:");
+        // await outputCallback((remain as any)._remainingMap);
     }
 );
