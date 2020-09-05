b0220499b11f35d7d40f95a319da840376ee6b84
gcali <gcali.git@gmailcom>
2020-09-05 14:58:30 +0200

    Add icon to calendar

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faSnowman,
+  faSnowflake,
+  faHollyBerry,
+  faGift,
+  faCandyCane,
+  faSleigh 
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+ 
+library.add(
+  faSnowman, 
+  faSnowflake, 
+  faHollyBerry, 
+  faGift, 
+  faCandyCane,
+  faSleigh
+);
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
