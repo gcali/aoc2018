d0c12393d5718913fe8379f9d589aa04d807886c
gcali <gcali.git@gmailcom>
2020-12-17 09:03:09 +0100

    Start day 17 2020

---
+++ src/entries/single-entries/2020/conway-cubes/index.ts
@@ -0,0 +1,11 @@
+import { entryForFile } from "../../../entry";
+
+export const conwayCubes = entryForFile(
+    async ({ lines, outputCallback, resultOutputCallback }) => {
+        throw Error("Not implemented");
+    },
+    async ({ lines, outputCallback, resultOutputCallback }) => {
+        throw Error("Not implemented");
+    },
+    { key: "conway-cubes", title: "Conway Cubes"}
+);
